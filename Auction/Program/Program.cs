b8d75f363faa08945560105b879086f6f5650748
Dmitry785 <danchenko.d2020@mail.ru>
2026-04-25 07:48:18 +0300

    add default data adding

--- Auction/Program/Program.cs
+++ Auction/Program/Program.cs
@@ -8,6 +8,7 @@ namespace Auction
 {
     public class Program
     {
+        private static readonly bool USE_DEF_DATA = false;
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
@@ -25,8 +26,9 @@ namespace Auction
 
             app.MapControllers();
 
-            using (var scope = app.Services.CreateScope())
-                scope.ServiceProvider.GetRequiredService<DefaultDataHelper>().AddDefaultData();
+            if(USE_DEF_DATA) 
+                using (var scope = app.Services.CreateScope())
+                    scope.ServiceProvider.GetRequiredService<DefaultDataHelper>().AddDefaultData();
 
             app.Run();
         }
@@ -41,24 +43,21 @@ public class DefaultDataHelper
     }
     public void AddDefaultData()
     {
+        _context.Database.EnsureDeleted();
+        _context.Database.EnsureCreated();
+
         var user1 = new User("u1", "u1 n", 111, new List<WalletCurrency>() { new WalletCurrency(1111, CurrencyType.RUB)});
         var user2 = new User("u2", "u2 n", 112, new List<WalletCurrency>() { new WalletCurrency(10, CurrencyType.RUB) });
         var item1 = new Item("1", "Item 1", "Item 1 desc", ItemType.Usual, user2);
         var item2 = new Item("2", "Item 2", "Item 2 desc", ItemType.Skin, user2);
         var item3 = new Item("3", "Item 3", "Item 3 desc", ItemType.Usual, user1);
         var lot1 = new Lot(item1, DateTime.Now, TimeSpan.FromHours(12), new Money(10, CurrencyType.RUB));
-        if(_context.Users.FirstOrDefault(x=>x.Id == user1.Id) is null)
-            _context.Users.Add(user1);
-        if (_context.Users.FirstOrDefault(x => x.Id == user2.Id) is null)
-            _context.Users.Add(user2);
-        if (_context.Items.FirstOrDefault(x => x.Id == item1.Id) is null)
-            _context.Items.Add(item1);
-        if (_context.Items.FirstOrDefault(x => x.Id == item2.Id) is null)
-            _context.Items.Add(item2);
-        if (_context.Items.FirstOrDefault(x => x.Id == item3.Id) is null)
-            _context.Items.Add(item3);
-        if (_context.Lots.FirstOrDefault(x => x.Id == lot1.Id) is null)
-            _context.Lots.Add(lot1);
+        _context.Users.Add(user1);
+        _context.Users.Add(user2);
+        _context.Items.Add(item1);
+        _context.Items.Add(item2);
+        _context.Items.Add(item3);
+        _context.Lots.Add(lot1);
         _context.SaveChanges();
     }
 }
